a35806e879ef619628a7039375534e74c1aa2f1f
Mathias Oberson <mathias.oberson@heig-vd.ch>
2017-06-27 10:49:50 +0200

    Implemented the requestAccess feature

--- src/ionicitude.service.js
+++ src/ionicitude.service.js
@@ -7,20 +7,21 @@ angular
 	.factory('Ionicitude', Ionicitude);
 
 /* @ngInject */
-function Ionicitude($q) {
+function Ionicitude($q, $log) {
 	/**
 	 * Defines the service object that will be publicly accessible when injecting the Ionicitude service.
 	 * @type {Object}
 	 */
 	var service = {
-		checkDevice: checkDevice,
+		checkDevice: checkDeviceFn,
+		requestAccess: requestAccessFn,
 		deviceSupportsFeatures: false,
-		init: init,
-		launchAR: launchAR,
-		addAction: addAction,
-		listLibActions: listLibActions,
-		captureScreen: captureScreen,
-		ready: ready,
+		init: initFn,
+		launchAR: launchArFn,
+		addAction: addActionFn,
+		listLibActions: listLibActionsFn,
+		captureScreen: captureScreenFn,
+		ready: readyFn,
 		// Wikitude API wrapper. Will be set by init()
 		close: null,
 		hide: null,
@@ -69,7 +70,7 @@ function Ionicitude($q) {
 		 * Additionnals world load config.
 		 * For now, there's only one such setting available for the Wikitude plugin : camera_position.
 		 */
-		worldLoadConfig: {camera_position: 'back'},
+		worldLoadConfig: { camera_position: 'back' },
 		/**
 		 * Defines which features your AR app needs the device to support.
 		 * For now, only two features are available in the cordova WikitudePlugin.
@@ -92,6 +93,29 @@ function Ionicitude($q) {
 	//////////////////// PUBLIC SERVICE METHODS ////////////////////
 
 	/**
+	 * This function can be used at anytime to ask the user for access permission.
+	 * The requested permissions depends upon the value of settings.reqFeatures.
+	 * The app will always ask for camera access (since it can't function at all without it).
+	 * The geolocation permission will be asked only if the settings.reqFeatures array contains the 'geo' string.
+	 * When the user accept all the request, the returned Promise's value will be an "OK" string.
+	 * If the user denied any request, or all of them, the returned Promise's value will be an object with a 'developerDescription'
+	 * and 'userDescription' properties, detailling in a String which permissions have been rejected. 
+	 * @return {Promise} A Promise of granted accesses or denied accesses.
+	 */
+	function requestAccessFn() {
+		$log.info('requesting access for', settings.reqFeatures);
+		return $q(function (resolve, reject) {
+			plugin.requestAccess(function success(data) {
+				$log.debug('requestAccess - success', data);
+				resolve(data);
+			}, function error(data) {
+				$log.error('requestAccess - error', data);
+				reject(data);
+			}, settings.reqFeatures);
+		});
+	}
+
+	/**
 	 * Should be used when wanting to call any sensitive Ionicitude's method - like launchAR() - while not being sure if the plugin will be fully initialized
 	 * by the time its method is called.
 	 * Since Ionicitude.init() is asynchronous when performing the device check, your AR World could be launch before the initialization process is fully resolved,
@@ -99,7 +123,7 @@ function Ionicitude($q) {
 	 * The parameter must be a function that should encapsulate the code you want to execute. Much like $ionicPlatform.ready() function.
 	 * @param {Function} request An anonymous function encapsulating the code to execute when Ionicitude's plugin is completely initialized.
 	 */
-	function ready(request) {
+	function readyFn(request) {
 		if (initialized && angular.isFunction(request)) request();
 		else requestsQueue.push(request);
 	}
@@ -107,8 +131,8 @@ function Ionicitude($q) {
 	/**
 	 * Debug method that returns a list of the current Actions that you added using the addAction() method.
 	 */
-	function listLibActions() {
-		console.log(Object.getOwnPropertyNames(lib));
+	function listLibActionsFn() {
+		$log.debug(Object.getOwnPropertyNames(lib));
 		return Object.getOwnPropertyNames(lib);
 	}
 
@@ -120,7 +144,7 @@ function Ionicitude($q) {
 	 * @param fileName If given, the screenshot will be saved in the device storage. If not, the screenshot will be saved in the device picture galery
 	 * @returns {Promise} A promise of a screen captured.
 	 */
-	function captureScreen(withUI, fileName) {
+	function captureScreenFn(withUI, fileName) {
 		if (typeof withUI !== 'boolean') throw new TypeError('Ionicitude - captureScreen() expects first parameter to be of type \'boolean\', \'' + typeof withUI + '\' given.');
 		if (fileName !== null && typeof fileName !== 'string') throw new TypeError('Ionicitude - captureScreen() expects second parameter to be \'null\' or of type \'string\', \'' + typeof fileName + '\' given.');
 		var q = $q.defer();
@@ -142,7 +166,7 @@ function Ionicitude($q) {
 	 * @throws TypeError
 	 * @throws SyntaxError
 	 */
-	function addAction(nameOrFunction, callback) {
+	function addActionFn(nameOrFunction, callback) {
 		if (angular.isString(nameOrFunction)) {
 			if (!callback) throw new TypeError('Ionicitude - addAction() expects a second argument if first argument is of type \'string\'.');
 			if (typeof callback !== 'function') throw new TypeError('Ionicitude - addAction() expects second argument to only be of type \'function\', \'' + typeof callback + '\' given.');
@@ -173,15 +197,15 @@ function Ionicitude($q) {
 	 * for it to be used later (alerting the user that he's/she's device is not compatible, for instance).
 	 * @return {Promise} A promise of a check result.
 	 */
-	function checkDevice() {
-		console.log('checking device');
+	function checkDeviceFn() {
+		$log.info('checking device');
 		var q = $q.defer();
 		plugin.isDeviceSupported(function (success) {
-			console.log(success);
+			$log.log(success);
 			service.deviceSupportsFeatures = true;
 			q.resolve(success);
 		}, function (error) {
-			console.log(error);
+			$log.error(error);
 			service.deviceSupportsFeatures = false;
 			q.reject(error);
 		}, settings.reqFeatures);
@@ -200,18 +224,18 @@ function Ionicitude($q) {
 	 * @param params An object to alter the init behavior or change default settings.
 	 * @returns {Promise} A promise of the initialization.
 	 */
-	function init(params) {
+	function initFn(params) {
 		return $q(function (resolve, reject) {
 			if (!initialized) {
-				console.log('init service starting');
+				$log.info('init service starting');
 				loadPlugin()
 					.then(setWrappers)
 					.then(setCallback)
 					.then(function () {
 						if (doDeviceCheck()) {
-							return checkDevice();
+							return checkDeviceFn();
 						} else {
-							return console.log('check skipped due to init settings');
+							return $log.info('check skipped due to init settings');
 						}
 					})
 					.then(handleSettings)
@@ -219,8 +243,8 @@ function Ionicitude($q) {
 						initialized = true;
 					})
 					.then(handleQueue)
-					.then(function() {
-						console.log('end of initialization');
+					.then(function () {
+						$log.info('end of initialization');
 						resolve(service);
 					})
 					.catch(function (error) {
@@ -298,11 +322,11 @@ function Ionicitude($q) {
 	 * @return {Function} A promise of a launch.
 	 * @throws UnsupportedFeatureError If the device does not support any feature requested by the AR World
 	 */
-	function launchAR(world_ref) {
+	function launchArFn(world_ref) {
 		if (!world_ref) world_ref = 'main';
 		if (service.deviceSupportsFeatures) {
 			var q = $q.defer();
-			console.log('launch');
+			$log.info('launch');
 			plugin.loadARchitectWorld(function (success) {
 				q.resolve(success);
 			}, function (error) {
@@ -320,7 +344,7 @@ function Ionicitude($q) {
 	 * Executes any request in the requestQueue, in the order they were stored, then reset the requestQueue
 	 */
 	function handleQueue() {
-		requestsQueue.forEach(function(request) {
+		requestsQueue.forEach(function (request) {
 			angular.isFunction(request) && request();
 		});
 		requestsQueue = [];
@@ -346,7 +370,7 @@ function Ionicitude($q) {
 	 */
 	function executeActionCall(call) {
 		var action = parseActionUrl(call);
-		console.log(call, action);
+		$log.debug(call, action);
 		if (!lib.hasOwnProperty(action.funcName) || typeof lib[action.funcName] !== 'function') throw new TypeError('\'' + action.funcName + '\' is either undefined or not a function in the Ionicitude service.');
 		lib[action.funcName](plugin, action.parameters);
 	}
